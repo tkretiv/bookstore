626e299031c6eceec181ae989b38ca0e94fcee15
Sebastian Börjesson <sebastianborjesson@Sebastians-MacBook-Pro.local>
2014-11-26 14:03:09 +0100

    fixat ett formulär för första user storyn

--- sql/book-questions.sql
+++ sql/book-questions.sql
@@ -3,3 +3,11 @@
 # username: root
 # password: mysql
 # database: bookstore
+
+# register book
+INSERT INTO books (isbn, name, author) 
+VALUES ({isbn}, {name}, {author});
+
+#register delivery
+INSERT INTO book_delivery (amount_delivery, f-price, bookshelf)
+VALUES ({amount_delivery}, {f-price}, {bookshelf});
