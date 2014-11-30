114449bf780cd05ea26d4b334df8f52c2835de13
Sebastian Börjesson <sebastianborjesson@Sebastians-MBP.lan>
2014-11-30 16:30:14 +0100

    fått våra knappar att fungera!

--- js/main.js
+++ js/main.js
@@ -42,35 +42,10 @@ $(function(){
       // run userLogin()
       success: function(data) {
       console.log("registerDelivery success: ", data);
-      getDelivery(deliveryInfo);
+      showInfoResult(deliveryInfo);
       },
       error: function(data) {
         console.log("registerDelivery failed: ", data);
-      }
-    });
-  }
-
-	function getDelivery(deliveryInfo) {
-	$.ajax({
-      // Use Nodebite's magic library
-      url:"libs/sql-ajax-json.php",
-      // Expect json in return
-      dataType: "json",
-      data: {
-        // Read SQL questions from this file
-        sql: "sql/book-questions.sql",
-        // Run the query named all products
-        run: "process delivery",
-        //data to send
-        isbn: JSON.stringify(deliveryInfo["isbn"]),
-        amount_delivery: JSON.stringify(deliveryInfo["amount_delivery"]),
-        fprice: JSON.stringify(deliveryInfo["fprice"]),
-        bookshelf: JSON.stringify(deliveryInfo["bookshelf"])
-      },
-      // When we have got an response from the server
-      // run userLogin()
-      success: function(data) {
-        console.log("getDelivery: ", data);
       }
     });
   }
@@ -114,6 +89,14 @@ $(function(){
 	});
 	return false;
 	});
+
+  $(".navtop button").click(function(){
+    $(".formParentWrapper").children().hide();
+
+    var thisBtnValue = $(this).val();
+
+      $(".formParentWrapper").children("."+thisBtnValue+"Form").show();
+  });
 
   $(".navbtm button").click(function(){
     $(".formParentWrapper").children().hide();
