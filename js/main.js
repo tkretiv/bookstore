9fee32c6a7e5a8631eb47ff23a3e7cb2cf083dc0
Sebastian Börjesson <sebastianborjesson@Sebastians-MBP.lan>
2014-11-30 12:40:36 +0100

    nu är inmatningsfunktionen klar!

---
+++ js/main.js
@@ -0,0 +1,117 @@
+$(function(){
+
+	function verifyDelivery(deliveryInfo) {
+	$.ajax({
+      // Use Nodebite's magic library
+      url:"libs/sql-ajax-json.php",
+      // Expect json in return
+      dataType: "json",
+      data: {
+        sql: "sql/book-questions.sql",
+        run: "get book",
+        isbn: JSON.stringify(deliveryInfo["isbn"])
+	},
+	success: function(data) {
+		console.log("verifyDelivery: ", data);
+    registerDelivery(deliveryInfo);
+        },
+	error: function(data){
+		console.log("You failed hard!");
+		}
+	});
+  }
+
+  function registerDelivery(deliveryInfo) {
+    $.ajax({
+      // Use Nodebite's magic library
+      url:"libs/sql-ajax-json.php",
+      // Expect json in return
+      dataType: "json",
+      data: {
+        // Read SQL questions from this file
+        sql: "sql/book-questions.sql",
+        // Run the query named all products
+        run: "register delivery",
+        //data to send
+        isbn: JSON.stringify(deliveryInfo["isbn"]),
+        amount_delivery: JSON.stringify(deliveryInfo["amount_delivery"]),
+        fprice: JSON.stringify(deliveryInfo["fprice"]),
+        bookshelf: JSON.stringify(deliveryInfo["bookshelf"])
+      },
+      // When we have got an response from the server
+      // run userLogin()
+      success: function(data) {
+      console.log("registerDelivery success: ", data);
+      getDelivery(deliveryInfo);
+      },
+      error: function(data) {
+        console.log("registerDelivery failed: ", data);
+      }
+    });
+  }
+
+	function getDelivery(deliveryInfo) {
+	$.ajax({
+      // Use Nodebite's magic library
+      url:"libs/sql-ajax-json.php",
+      // Expect json in return
+      dataType: "json",
+      data: {
+        // Read SQL questions from this file
+        sql: "sql/book-questions.sql",
+        // Run the query named all products
+        run: "process delivery",
+        //data to send
+        isbn: JSON.stringify(deliveryInfo["isbn"]),
+        amount_delivery: JSON.stringify(deliveryInfo["amount_delivery"]),
+        fprice: JSON.stringify(deliveryInfo["fprice"]),
+        bookshelf: JSON.stringify(deliveryInfo["bookshelf"])
+      },
+      // When we have got an response from the server
+      // run userLogin()
+      success: function(data) {
+        console.log("getDelivery: ", data);
+      }
+    });
+  }
+
+
+	$(".func .deliveryInfo").submit(function() {
+
+	var deliveryInfo = {};
+
+		$(this).find("input").not("input[type='submit']").each(function() {
+      deliveryInfo[this.name] = $(this).val();
+    });
+
+
+
+		console.log("deliveryInfo: ", deliveryInfo);
+
+
+	$.ajax({
+      // Use Nodebite's magic library
+      url:"libs/sql-ajax-json.php",
+      // Expect json in return
+      dataType: "json",
+      data: {
+        sql: "sql/book-questions.sql",
+        run: "register book",
+        isbn: JSON.stringify(deliveryInfo["isbn"]),
+        name: JSON.stringify(deliveryInfo["name"]),
+        author: JSON.stringify(deliveryInfo["author"])
+	},
+	success: function(data) {
+		console.log("1. form successfully submitted!");
+		console.log("data: ", data);
+    registerDelivery (deliveryInfo);
+		
+	},
+	error: function(data){
+		console.log("error: ", data);
+	}
+		
+	});
+	return false;
+	});
+});
--- sql/book-questions.sql
+++ sql/book-questions.sql
@@ -2,12 +2,22 @@
 # hostname: 127.0.0.1
 # username: root
 # password: mysql
-# database: bookstore
+# database: bookshop
 
 # register book
-INSERT INTO books (isbn, name, author) 
-VALUES ({isbn}, {name}, {author});
+INSERT INTO books (isbn, name, author) VALUES ({isbn}, {name}, {author});
 
-#register delivery
-INSERT INTO book_delivery (amount_delivery, f-price, bookshelf)
-VALUES ({amount_delivery}, {f-price}, {bookshelf});
+# get book
+SELECT * FROM books, book_delivery WHERE isbn={isbn};
+
+# register delivery
+INSERT INTO book_delivery (isbn, amount_delivery, fprice, bookshelf) VALUES ({isbn}, {amount_delivery}, {fprice}, {bookshelf}); 
+
+# process delivery
+SELECT books.isbn, books.name, books.author 
+FROM books, book_delivery WHERE book_delivery.isbn={isbn} && book_delivery.amount_delivery={amount_delivery} && book_delivery.fprice={fprice} && book_delivery.bookshelf={bookshelf};
+
+# show delivery
+SELECT books.isbn, books.name, books.author, book_delivery.amount_delivery, book_delivery.fprice, book_delivery.bookshelf 
+FROM books, book_delivery WHERE book_delivery.isbn=books.isbn;
+
