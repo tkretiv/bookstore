c4461353e41f3d036dc1db323cc53921d5c38dda
tkretiv <tkretiv@gmail.com>
2014-12-01 13:53:55 +0100

    form för kundsök; sql för kundsök

--- sql/book-questions.sql
+++ sql/book-questions.sql
@@ -21,3 +21,19 @@ FROM books, book_delivery WHERE book_delivery.isbn={isbn} && book_delivery.amoun
 SELECT books.isbn, books.name, books.author, book_delivery.amount_delivery, book_delivery.fprice, book_delivery.bookshelf 
 FROM books, book_delivery WHERE book_delivery.isbn=books.isbn;
 
+#kundsearching
+Select b.isbn, b.name, b.author, 
+	(select kund_price 
+	from history_kundprice hk 
+	where hk.isbn = b.isbn 
+	order by data_change desc 
+	limit 1) kund_price, 
+	(select sum(amount) 
+	from (select isbn, amount_delivery amount 
+			from book_delivery 
+			union select isbn, -amount_sale 
+			from book_sales ) t
+	 where t.isbn=b.isbn 
+	 group by isbn) bookstore_balance 
+from books b 
+where isbn like {isbn} and upper(author) like upper({author}) and upper(name) like upper({name});
